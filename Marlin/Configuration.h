9a235db68a2891731c2188eb6da15db2f5615bca
Christian Würthner <c.wurthner@gmail.com>
2020-02-15 19:10:35 +0100

    Set preheat configurations

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1431,14 +1431,14 @@
 
 // Preheat Constants
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 185
-#define PREHEAT_1_TEMP_BED     45
+#define PREHEAT_1_TEMP_HOTEND 200
+#define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
 
-#define PREHEAT_2_LABEL       "ABS"
-#define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED    110
-#define PREHEAT_2_FAN_SPEED   255 // Value from 0 to 255
+#define PREHEAT_2_LABEL       "CPE"
+#define PREHEAT_2_TEMP_HOTEND 265
+#define PREHEAT_2_TEMP_BED    75
+#define PREHEAT_2_FAN_SPEED   64 // Value from 0 to 255
 
 /**
  * Nozzle Park
