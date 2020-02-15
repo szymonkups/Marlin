a69b0c6624a530d62af20e01ca022593b4a994b6
Christian Würthner <c.wurthner@gmail.com>
2020-02-15 17:46:50 +0100

    Update NOZZLE_TO_PROBE_OFFSET to measured value

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -914,7 +914,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -40, -10, -1.85 }
+#define NOZZLE_TO_PROBE_OFFSET { -40, -10, -2.33 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
