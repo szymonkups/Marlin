896574cd73f4044eb6566c79bb62bf1f2553292b
Szymon <szymonkups@gmail.com>
2020-03-06 16:17:36 +0100

    New BLTouch offset after switching to Micro-Swiss hotend

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -989,7 +989,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -41, -10, -1.78 }
+#define NOZZLE_TO_PROBE_OFFSET { -41, -10, -1.43 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
